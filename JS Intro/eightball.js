8abbe72aded163c63a7323b7010267f3e540aba2
anthony28 <anthony@wearea2.com>
2019-06-27 14:16:49 +0300

    added eghtball excercise

---
+++ JS Intro/eightball.js
@@ -0,0 +1,44 @@
+let userName = 'Miller';
+userName.length > 1 ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+
+let userQuestion = 'Ask your question here';
+
+console.log(`${userName} asks: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = randomNumber;
+
+
+switch(eightBall) {
+  case 0:
+    	console.log('It is certain');
+    	break;
+  case 1:
+    	console.log('It is decidedly so');
+    	break;
+  case 2:
+    	console.log('Reply hazy try again');
+    	break;
+  case 3:
+    	console.log('Cannot predict now');
+    	break;
+  case 4:
+    	console.log('Do not count on it');
+    	break;
+  case 5:
+    	console.log('My sources say no');
+    	break;
+  case 6:
+    	console.log('Outlook not so good');
+    	break;
+  case 7:
+    	console.log('Signs point to yes');
+    	break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
+
+
+
